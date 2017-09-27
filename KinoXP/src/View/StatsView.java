e04fead7b05af48b0404a8845dff519ddc02d1a2
hamzah0553 <hamzah1996@hotmail.com>
2017-09-27 15:00:56 +0200

    working pane

--- KinoXP/src/View/StatsView.java
+++ KinoXP/src/View/StatsView.java
@@ -5,12 +5,14 @@ import System.View;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import javafx.scene.control.TableView;
 
 /**
  * Created by hamza on 27-09-2017.
@@ -24,16 +26,22 @@ public class StatsView extends View {
 
     public Pane centerPane()
     {
-        GridPane gridPane = new GridPane();
+        TableView table = new TableView();
+        TableColumn movie = new TableColumn("Movies");
+        TableColumn sales = new TableColumn("Sales");
 
-        for (int i = 0; i < 10; i++)
-        {
-            gridPane.add(new Text("Movie"), 0, i);
-            gridPane.add(new Text("25"), 1, i);
-        }
+        String test = "Test";
 
-        gridPane.setPadding(new Insets(25, 25, 25, 25));
-        gridPane.setAlignment(Pos.CENTER);
-        return gridPane;
+
+        table.getColumns().addAll(movie, sales);
+        TableRow tableRow = new TableRow();
+        tableRow.setText(test);
+
+        Pane pane = new Pane();
+        pane.getChildren().addAll(table);
+
+        pane.setPadding(new Insets(25, 25, 25, 25));
+
+        return pane;
     }
 }
