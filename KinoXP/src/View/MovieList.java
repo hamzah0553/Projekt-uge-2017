8113739ba862fd5802f5165e5d84e19358d40673
hamzah0553 <hamzah1996@hotmail.com>
2017-09-28 11:39:17 +0200

    BEAUTIFUL MOVIELIST

--- KinoXP/src/View/MovieList.java
+++ KinoXP/src/View/MovieList.java
@@ -1,23 +1,20 @@
 package View;
 
-import DataAccessObject.AddMovieDAO;
+import DataAccessObject.FetchMovieListDAO;
 import Models.Movie;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import System.View;
 
@@ -45,13 +42,26 @@ public class MovieList extends View{
         {
             imageViews.add(new ImageView(new Image("https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg")));
             buttons.add(new Button("Find spilletider"));
-            imageViews.get(i).setFitHeight(480);
-            imageViews.get(i).setFitWidth(252);
+            imageViews.get(i).setFitHeight(455);
+            imageViews.get(i).setFitWidth(238.875);
         }
 
         gridPane.setHgap(25);
         gridPane.setVgap(15);
 
+        window.setTitle("Movies");
+
+        FetchMovieListDAO dao = new FetchMovieListDAO();
+        ArrayList<Movie> movies = dao.getMovies();
+
+        for (Movie m :
+                movies) {
+
+            if (m.getRun()==1)
+            {
+
+            }
+        }
 
         int row = 0;
         int column = 0;
@@ -67,8 +77,8 @@ public class MovieList extends View{
 
             if(column == 3)
             {
-               row = row + 2;
-               column = 0;
+                row = row + 2;
+                column = 0;
             }
 
             gridPane.add(buttons.get(i), movieColumn, movieRow);
@@ -92,11 +102,10 @@ public class MovieList extends View{
 
         pane.setPadding(new Insets(25, 0, 25, 0));
 
+
         pane.setFitToHeight(true);
         pane.setFitToWidth(true);
 
-        pane.setHvalue(0.5);
-        pane.setVvalue(0.5);
 
         return pane;
     }
@@ -105,26 +114,48 @@ public class MovieList extends View{
     {
         FlowPane flowPane = new FlowPane();
 
-        Button newPlay = new Button("Ny Spilletid");
-        Button showSale = new Button("Stats for film");
-        Button newMovie = new Button("Ny film");
+        Button newPlay = new Button("Opret Spilletid");
+        Button showSale = new Button("Statistikker");
+        Button newMovie = new Button("Opret film");
+
+        newPlay.getStyleClass().add("btn");
+        newPlay.getStyleClass().add("non");
+
+
+
+        showSale.getStyleClass().add("btn");
+        showSale.getStyleClass().add("non");
+
+        newMovie.getStyleClass().add("btn");
+        newMovie.getStyleClass().add("non");
+
+        flowPane.setAlignment(Pos.CENTER);
+
+        //flowPane.setStyle("-fx-background-color: white");
+
+        flowPane.getStyleClass().add("bottomPane");
 
         flowPane.setHgap(5.0);
 
+        flowPane.getStylesheets().add("css/style.css");
+
         flowPane.getChildren().addAll(newPlay, showSale, newMovie);
 
-        newPlay.setOnAction(event -> {
+        newPlay.setOnAction(event ->
+        {
             PlayListCrud playListCrud = new PlayListCrud(primaryStage);
             playListCrud.setScene(playListCrud.layout(), null, primaryStage);
         });
 
-        showSale.setOnAction(event -> {
+        showSale.setOnAction(event ->
+        {
             StatsView statsView = new StatsView();
-            statsView.setScene(statsView.centerPane(), null,primaryStage);
+            statsView.setScene(statsView.centerPane(), null, primaryStage);
         });
 
-        newMovie.setOnAction(event -> {
-            AddMovieDAO dao = new AddMovieDAO();
+        newMovie.setOnAction(event ->
+        {
+            FetchMovieListDAO dao = new FetchMovieListDAO();
             dao.getMovies();
             CreateMovies createMovies = new CreateMovies(primaryStage);
         });
